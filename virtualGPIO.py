8837fd25a6579921fd01ff52fae2ebc4a659a55b
Daniel-Domitian Iuonac <dd.iuonac@gmail.com>
2019-05-16 09:35:55 +0300

    Functions are tested. You may need to sleep 1.6 seconds between commands.

--- virtualGPIO.py
+++ virtualGPIO.py
@@ -6,11 +6,12 @@ PORT = "COM3"
 BAUD_RATE = 9600
 STD_TIMEOUT = 0.8
 
-
+# DIGITAL PINS VALUE
 HIGH = 2
 LOW = 1
 
-A0 = 14   # the analog pins
+# ANALOG PINS
+A0 = 14
 A1 = 15
 A2 = 16
 A3 = 17
@@ -19,22 +20,28 @@ A5 = 19
 A6 = 20
 A7 = 21
 
+# PIN MODE
 OUT = 2
 IN = 1
 
-Serial = serial.Serial(PORT, BAUD_RATE, timeout=STD_TIMEOUT, dsrdtr=False)
+Serial = serial.Serial(PORT, BAUD_RATE, timeout=STD_TIMEOUT,  dsrdtr=False)
 
 
 def _serial_write(message: str):
-    Serial.write(message.encode("utf-8"))
+    data = message.encode("utf-8")
+    print("Sending: ", data)
+    Serial.write(data)
 
 
 def _serial_read(count=1):
-    return Serial.read(count)
+    data = Serial.read(count)
+    print("Received: ", data)
+    return data
 
 
 def setup(pin: int, mode: int):
     _serial_write("1/" + str(pin) + "/" + str(mode) + "/")
+    time.sleep(STD_TIMEOUT)
 
 
 def digital_read(pin: int):
@@ -46,13 +53,16 @@ def digital_read(pin: int):
 def analog_read(pin: int):
     _serial_write("3/1/" + str(pin) + "/")
     time.sleep(STD_TIMEOUT)
-    return _serial_read()
+    return _serial_read(count=8)
 
 
 def digital_write(pin: int, value: int):
     _serial_write("2/2/" + str(pin) + "/" + str(value) + "/")
+    time.sleep(STD_TIMEOUT)
 
 
 def analog_write(pin: int, value: int):
     _serial_write("2/1/" + str(pin) + "/" + str(value) + "/")
+    time.sleep(STD_TIMEOUT)
+
 
